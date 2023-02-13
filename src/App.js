02ea80bd4556c469f4a4b52854633595fe55f595
Amirreza <amirreza.m13825@gmail.com>
2023-02-13 23:05:05 +0330

    Route and Switch Components added from rrd package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/HomePage";
+import CartPage from "./pages/CartPage";
 
 function App() {
   return (
-    <div className="App">
-      <HomePage />
-    </div>
+    <Switch>
+      <div className="App">
+        <Route path={"/cart"} component={CartPage} />
+        <Route exact path={"/"} component={HomePage} />
+      </div>
+    </Switch>
   );
 }
 

e7083d0ab90fe755f28f15e1fb47d98ee5bca169
Amirreza <amirreza.m13825@gmail.com>
2023-02-13 23:04:39 +0330

    CartPage created

---
+++ src/pages/CartPage.jsx
@@ -0,0 +1,11 @@
+import Layout from "../Layout/Layout";
+
+const CartPage = () => {
+  return (
+    <Layout>
+      <h2>This is cart page !</h2>
+    </Layout>
+  );
+};
+
+export default CartPage;

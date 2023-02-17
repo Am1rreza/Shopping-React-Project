8656ea28a3a1158735dd8fe380e3fb556d6b8d41
Amirreza <amirreza.m13825@gmail.com>
2023-02-17 23:18:28 +0330

    products added and their styles added too

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,26 @@
 import Layout from "../Layout/Layout";
+import * as data from "../data";
 
 const HomePage = () => {
   return (
     <Layout>
-      <h2>This is home page !</h2>
+      <main className="container">
+        <section className="productList">
+          {data.products.map((product) => {
+            return (
+              <section className="product" key={product.id}>
+                <div className="productImg">
+                  <img src={product.image} alt={product.name} />
+                </div>
+                <div className="productDescription">
+                  <p>{product.name}</p>
+                  <p>{product.price}$</p>
+                </div>
+              </section>
+            );
+          })}
+        </section>
+      </main>
     </Layout>
   );
 };

a7cd95210315a843d219308e592e01cac5639ae5
Amirreza <amirreza.m13825@gmail.com>
2023-02-18 18:52:43 +0330

    load products in cart page

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,9 +1,28 @@
+import { Link } from "react-router-dom";
 import Layout from "../Layout/Layout";
+import { useCart } from "../Providers/CartProvider";
 
 const CartPage = () => {
+  const { cart } = useCart();
+
   return (
     <Layout>
-      <h2>This is cart page !</h2>
+      <main>
+        {cart.length ? (
+          cart.map((item) => {
+            return (
+              <div key={item.id}>
+                <p>{item.name}</p>
+              </div>
+            );
+          })
+        ) : (
+          <div>
+            <h3>There is no product in the cart !</h3>
+            <Link to={"/"}>Add some Products</Link>
+          </div>
+        )}
+      </main>
     </Layout>
   );
 };

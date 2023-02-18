eb0404f5c299b092d59da905f0342428691ef0de
Amirreza <amirreza.m13825@gmail.com>
2023-02-18 22:46:20 +0330

    refactore conditional rendering

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,27 +1,48 @@
 import { Link } from "react-router-dom";
 import Layout from "../Layout/Layout";
 import { useCart } from "../Providers/CartProvider";
+import "./cartPage.css";
 
 const CartPage = () => {
   const { cart } = useCart();
 
+  // Conditional rendering
+  if (!cart.length)
+    return (
+      <Layout>
+        <div className="emptyCart">
+          <div>
+            <h3>There is no product in your cart !</h3>
+            <Link className="btn secondaryBtn" to={"/"}>
+              Add some Products
+            </Link>
+          </div>
+        </div>
+      </Layout>
+    );
+
   return (
     <Layout>
-      <main>
-        {cart.length ? (
-          cart.map((item) => {
+      <main className="container">
+        <section className="cartItemList">
+          {cart.map((item) => {
             return (
-              <div key={item.id}>
-                <p>{item.name}</p>
+              <div className="cartItem" key={item.id}>
+                <div className="itemImg">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div>{item.name}</div>
+                <div>{item.price * item.quantity}$</div>
+                <div>
+                  <button>Add</button>
+                  <button>{item.quantity}</button>
+                  <button>Remove</button>
+                </div>
               </div>
             );
-          })
-        ) : (
-          <div>
-            <h3>There is no product in the cart !</h3>
-            <Link to={"/"}>Add some Products</Link>
-          </div>
-        )}
+          })}
+        </section>
+        <section className="cartSummary">cart summary</section>
       </main>
     </Layout>
   );

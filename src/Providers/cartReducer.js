b19359a86390e05f77ba680a92b796785ab25ffb
Amirreza <amirreza.m13825@gmail.com>
2023-02-21 23:51:28 +0330

    refactor cartReducer

--- src/Providers/cartReducer.js
+++ src/Providers/cartReducer.js
@@ -1,57 +1,60 @@
+const addToCart = (state, action) => {
+  const updatedCart = [...state.cart];
+
+  const index = updatedCart.findIndex((item) => item.id === action.payload.id);
+
+  if (index < 0) {
+    updatedCart.push({ ...action.payload, quantity: 1 });
+  } else {
+    const item = { ...updatedCart[index] };
+    item.quantity++;
+    updatedCart[index] = item;
+  }
+
+  return {
+    ...state,
+    cart: updatedCart,
+    total: state.total + action.payload.price,
+  };
+};
+
+const decrementProduct = (state, action) => {
+  const updatedCart = [...state.cart];
+
+  const index = updatedCart.findIndex((item) => item.id === action.payload.id);
+
+  const item = { ...updatedCart[index] };
+
+  if (item.quantity === 1) {
+    const filteredProducts = updatedCart.filter(
+      (product) => product.id !== item.id
+    );
+
+    return {
+      ...state,
+      cart: filteredProducts,
+      total: state.total - action.payload.price,
+    };
+  } else {
+    item.quantity--;
+    updatedCart[index] = item;
+
+    return {
+      ...state,
+      cart: updatedCart,
+      total: state.total - action.payload.price,
+    };
+  }
+};
+
 const cartReducer = (state, action) => {
   switch (action.type) {
-    case "ADD_TO_CART": {
-      const updatedCart = [...state.cart];
+    case "ADD_TO_CART":
+      return addToCart(state, action);
 
-      const index = updatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
+    case "DECREMENT_PRODUCT":
+      return decrementProduct(state, action);
 
-      if (index < 0) {
-        updatedCart.push({ ...action.payload, quantity: 1 });
-      } else {
-        const item = { ...updatedCart[index] };
-        item.quantity++;
-        updatedCart[index] = item;
-      }
-
-      return {
-        ...state,
-        cart: updatedCart,
-        total: state.total + action.payload.price,
-      };
-    }
-
-    case "DECREMENT_PRODUCT": {
-      const updatedCart = [...state.cart];
-
-      const index = updatedCart.findIndex(
-        (item) => item.id === action.payload.id
-      );
-
-      const item = { ...updatedCart[index] };
-
-      if (item.quantity === 1) {
-        const filteredProducts = updatedCart.filter(
-          (product) => product.id !== item.id
-        );
-
-        return {
-          ...state,
-          cart: filteredProducts,
-          total: state.total - action.payload.price,
-        };
-      } else {
-        item.quantity--;
-        updatedCart[index] = item;
-
-        return {
-          ...state,
-          cart: updatedCart,
-          total: state.total - action.payload.price,
-        };
-      }
-    }
     default:
       return state;
   }
